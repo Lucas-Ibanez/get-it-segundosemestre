ad8c072f85808146dbc0ebf0d7dacc769d65e851
LucasIbanez <lucas.ibanez.ismart@gmail.com>
2026-08-13 16:52:58 -0300

    Parte 4: Fazendo um formulário de criação de anotação

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from utils import load_data, load_template
+import json
 
 def index():
     note_template = load_template('components/note.html')
@@ -9,3 +10,10 @@ def index():
     notes = '\n'.join(notes_li)
 
     return load_template('index.html').format(notes=notes)
+
+def submit(titulo, detalhes):
+    json_conteudo = list(load_data('notes.json'))
+    json_conteudo.append({'titulo': titulo, 'detalhes': detalhes})
+
+    with open("static/data/notes.json", "w") as arquivo_json:
+        json.dump(json_conteudo, arquivo_json)
